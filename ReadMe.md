a830adc4dec60ddbc022b16871105524afede930
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-10-19 14:18:43 +0100

    Initial Check-in

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# MonoGame Skinned Model example
+
+Upgraded Skinned Model example from the XNA Game Studio Archive
+
+Source:
+
+https://github.com/simondarksidej/XNAGameStudio/wiki/Skinned-Model
---
+++ SkinningSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SkinningSample
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new SkinningSampleGame())
+                game.Run();
+        }
+    }
+}
